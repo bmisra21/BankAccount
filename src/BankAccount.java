25c1a1f3d8d28f222ab5014d1869f2832b7f7346
bmisr <bmisr@DESKTOP-J3KHLGD.fios-router.home>
2018-09-26 20:28:16 -0400

    Push1 from home

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,10 +1,13 @@
+// @author: bmisra21 (Bharavi Misra Period 6)
+
+
 
 public class BankAccount 
 {
 	//fields
 	private String name;
 	private int accNum;
-	private int balance;
+	private double balance;
 	
 	//constructors
 	public BankAccount(String n, int num)
@@ -13,17 +16,28 @@ public class BankAccount
 		accNum = num;
 		balance = 0;
 	}
-	 public BankAccount (String n, int num, int b)
+	 public BankAccount (String n, int num, double bal)
 	 {
 		name = n;
 		accNum = num;
-		balance = b;
+		balance = bal;
 	 }
 	 
 	 //methods
-	 
-	 public int deposit (int a)
+	 public void deposit (double a)
 	 {
-		 return 0;
+		 balance += a;
+	 }
+	 public void withdraw (double a)
+	 {
+		 balance -= a;
+	 }
+	 public double getBalance ()
+	 {
+		 return balance;
+	 }
+	 public String toString ()
+	 {
+		 return name+" "+accNum+" "+balance;
 	 }
 }
---
+++ src/BankAccountMain.java
@@ -0,0 +1,11 @@
+
+public class BankAccountMain {
+
+	public static void main(String[] args) 
+	{
+		BankAccount acc1 = new BankAccount("bob", 1);
+		System.out.println(acc1.toString());
+
+	}
+
+}
